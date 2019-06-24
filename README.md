0ecf6f7485deb9d07844b050459fbd6eac75e060
Rdyx <tira.nicolas@gmail.com>
2019-06-24 11:17:46 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Bash user input and rand
+
+Simple light weighted script to ensure a user fill a required field in bash.
+You can also hide what's user typing by using the PASSWORD value.
+The rand function is just a bonus to generate random numbers between 100000 and 999999.
+
+Please notice that no check is done on what the user is entering.
+
+Text will be shown like this:
+```bash
+Question [default-value]: {user-input} # user-input is hidden if password arg is set.
+```
+
+# How to use ?
+Copy-paste the script where you want to use it and source it in the bash you want (```. utils.sh```)
+P.s: . is used rather than source because it works in bash & sh.
+
+```bash
+# My first question: {user-input}
+TEST1=$(REQUIRED_FIELD "My first question")
+# {user-input}
+echo $TEST1
+
+# Required field with default value
+# My second question [default-value]: {user-input}
+TEST2=$(REQUIRED_FIELD "My second question" "default-value" DEFAULT)
+# {default-value/user-input}
+echo $TEST2
+
+# Password field
+# My password: {hidden-user-input}
+TEST3=$(REQUIRED_FIELD "My password" "" NO_DEFAULT PASSWORD)
+# {hidden-user-input}
+echo $TEST3
+
+# Password field with default random value
+# My password [random-number]: {hidden-user-input}
+TEST4=$(REQUIRED_FIELD "My password" "$(rand)" DEFAULT PASSWORD)
+# {random-number/hidden-user-input}
+echo $TEST4
+
+```
+
---
+++ utils.sh
@@ -0,0 +1,65 @@
+# IN BASH, CONDITIONS RETURN TRUE=0 AND FALSE=1
+
+##### UTILS #####
+# SET DEFAULT VALUE FOR USER INPUT
+function DEFAULT_VALUE () {
+    if [[ $IS_PASSWORD = 0 ]]; then SILENCE_INPUT='-s'; fi
+
+    read -p "$QUESTION [$DEFAULT_INPUT_VALUE] (Leave blank to use default value): " $SILENCE_INPUT USER_INPUT # Ask the user to enter a string and show a default input value in []
+    USER_INPUT=${USER_INPUT:-$DEFAULT_INPUT_VALUE} # Is user doesn't input, assign DEFAULT_INPUT_VALUE
+
+    if [[ $IS_PASSWORD = 0 ]]; then echo '' >&2; fi 
+    echo $USER_INPUT # Returning value
+}
+
+# SPECIAL INPUT STATUS (such as default value and/or password)
+function SPECIAL_STATUS () {
+    # If field is checked to have a default value and is password
+    local USER_DEFAULT_INPUT=$1 # Get default input value
+    if [[ $IS_DEFAULT = 0 ]] && [[ $IS_PASSWORD = 0 ]]; then
+        USER_INPUT=$(DEFAULT_VALUE $QUESTION $USER_DEFAULT_INPUT) # Ask the user to enter a string or leave it empty to keep default value
+
+    elif [[ $IS_DEFAULT = 0 ]]; then
+        USER_INPUT=$(DEFAULT_VALUE $QUESTION $USER_DEFAULT_INPUT) # Ask the user to enter a string or leave it empty to keep default value
+
+    elif [[ $IS_PASSWORD = 0 ]]; then
+        read -p "$QUESTION: " -s USER_INPUT # Ask the user to enter a password (-s to hide input)
+
+    else
+        read -p "$QUESTION: " USER_INPUT # Ask the user to enter a string
+    fi
+
+    echo $USER_INPUT
+}
+
+# SET THE FIELD AS "REQUIRED"
+function REQUIRED_FIELD () {
+    local QUESTION=$1 # Show question
+    local DEFAULT_INPUT_VALUE=$2 # Get default input value
+
+    # Check if field is supposed to have a default value
+    [[ $3 = 'DEFAULT' ]]
+    local IS_DEFAULT=$? 
+    
+    # Check if field is password field
+    [[ $4 == 'PASSWORD' ]]
+    local IS_PASSWORD=$? 
+
+    local USER_INPUT=$(SPECIAL_STATUS $DEFAULT_INPUT_VALUE $IS_DEFAULT $IS_PASSWORD)
+
+    # If field is required, ask user untill he force stops or he enters a value
+    while  [[ -z $USER_INPUT ]] # While string is empty...
+    do
+        echo "This field is required." >&2 # Ask the user to enter a valid string (>&2 if for direct echo output)
+        USER_INPUT=$(SPECIAL_STATUS $DEFAULT_INPUT_VALUE $IS_DEFAULT $IS_PASSWORD)
+    done 
+
+    echo '' >&2
+    echo $USER_INPUT # Returning value of user input
+}
+
+# Generating random number between 100000 and 999999 to avoid duplicate project names
+function rand () {
+    echo $(awk -v seed="$RANDOM" 'BEGIN{srand(seed);print int(rand()*(999999-100000))+100000 }')
+}
+##### UTILS #####
